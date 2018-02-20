b2190521049dc55f718d0b60e1d738f72e538914
Tim Swast <swast@google.com>
2018-02-19 17:23:51 -0800

    Add development server.

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -5,5 +5,11 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  // Don't use in production.
+  // https://webpack.js.org/guides/development/
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
   }
 }
